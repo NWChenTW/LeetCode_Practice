6b02f3e2361446fcc51cd071b3422808b6d4007e
nwcchen <arsimiller@yahoo.com.tw>
2020-06-08 23:59:22 -0500

    217 060820

---
+++ 217. Contains Duplicate/C.c
@@ -0,0 +1,9 @@
+int cmp(const void *a, const void *b){
+    return *(const int *)a - *(const int *)b;
+}
+
+bool containsDuplicate(int* nums, int numsSize){
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (; numsSize>1 && nums[0] != nums[1]; numsSize--, nums++);
+    return numsSize > 1;
+}ㄕ
